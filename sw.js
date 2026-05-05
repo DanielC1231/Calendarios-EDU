24654cd40535b2a4a138c2ca77a90afcf0defde8
DanielC1231 <cuellar.daniel@juanpablo2.edu.mx>
2026-05-05 15:12:22 -0700

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -3,12 +3,9 @@ const REPO_NAME = '/Noticias-EDU';
 
 const urlsToCache = [
     REPO_NAME + '/',
-    REPO_NAME + '/index.html',
-    REPO_NAME + '/style.css',
-    REPO_NAME + '/manifest.json'
+    REPO_NAME + '/index.html'
 ];
 
-// Instalar y guardar en caché
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -19,7 +16,6 @@ self.addEventListener('install', function(event) {
     self.skipWaiting();
 });
 
-// Activar
 self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
@@ -35,7 +31,6 @@ self.addEventListener('activate', function(event) {
     self.clients.claim();
 });
 
-// Responder con caché o red
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
@@ -48,22 +43,6 @@ self.addEventListener('fetch', function(event) {
     );
 });
 
-// MANEJAR NOTIFICACIONES PUSH (para OneSignal o notificaciones locales)
-self.addEventListener('push', function(event) {
-    if (event.data) {
-        const data = event.data.json();
-        event.waitUntil(
-            self.registration.showNotification(data.title || 'Nueva Noticia', {
-                body: data.body || 'Hay contenido nuevo disponible',
-                icon: 'https://cdn-icons-png.flaticon.com/512/9162/9162399.png',
-                badge: 'https://cdn-icons-png.flaticon.com/512/9162/9162399.png',
-                vibrate: [200, 100, 200]
-            })
-        );
-    }
-});
-
-// MANEJAR CLICK EN NOTIFICACIÓN
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
     event.waitUntil(
